a01f411aceca42ef1d7ded45e4a8cec4340727e8
Kirill <forestblackone@gmail.com>
2022-11-14 22:31:13 +0300

    8 great

---
+++ 8/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 8 Вывести на экран числа от -N до N
+
+Console.WriteLine("Введите число N: ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+int index = 0;
+int value = 0;
+while(index < n){
+    index = n + (value - n * 2);
+    Console.Write(index + " ");
+    value++;
+}
