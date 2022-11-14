509d4cc465e2134f987c99691bee8ee30ed54a3f
Kirill <forestblackone@gmail.com>
2022-11-14 22:00:11 +0300

    7 great

--- 7/Program.cs
+++ 7/Program.cs
@@ -1,2 +1,10 @@
 ﻿// Задача 7 Выяснить является ли число чётным
 
+Console.WriteLine("Введите целое число: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+if(a % 2 == 0){
+    Console.WriteLine("Число четное.");
+}
+else{ Console.WriteLine("Число нечетное.");}
